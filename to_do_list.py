33c73d704d24b91d77702ebd03f771137db10f59
Faramarz Hosseini <pitofsoran33@gmail.com>
2021-03-18 23:46:19 +0330

    Added edit task button

--- to_do_list.py
+++ to_do_list.py
@@ -2,6 +2,7 @@ import tkinter
 import tkinter.messagebox
 import pickle
 
+
 root = tkinter.Tk()
 root.title('To Do List BY a_walking_dead')
 
@@ -23,6 +24,23 @@ class Task:
             Initializer.entry_task.delete(0, tkinter.END)
         else:
             tkinter.messagebox.showwarning(title="WARNING", message="Please enter a task.")
+
+    @staticmethod
+    def edit_task():
+        old_task = Task.select_task()
+        new_task = Task.get_task()
+        if new_task != "":
+            try:
+                selection_value = Initializer.listbox_tasks.get(Initializer.listbox_tasks.curselection())
+                lst = list(Initializer.listbox_tasks.get(0, "end"))
+                selection_index = lst.index(selection_value)
+                Initializer.listbox_tasks.delete(old_task)
+                Initializer.listbox_tasks.insert(selection_index, new_task)
+                Initializer.entry_task.delete(0, tkinter.END)
+            except:
+                tkinter.messagebox.showwarning(title="CAUTION", message="Please select the task you wish to edit first.")
+        else:
+            tkinter.messagebox.showwarning(title="CAUTION", message="Please type in the new task.")
 
     @staticmethod
     def del_task():
@@ -54,7 +72,7 @@ class Task:
             if successful_load:
                 tkinter.messagebox.showinfo(title="STATUS", message="Tasks successfully loaded.")
         except:
-            tkinter.messagebox.showerror(title="ERROR!", message="No save file to load.")
+            tkinter.messagebox.showerror(title="ERROR!", message="No tasks to load.")
 
 
 # GUI
@@ -73,6 +91,8 @@ class Gui:
         # Assets > Buttons
         self.button_add_task = tkinter.Button(root, text='+Task', width=self.listbox_tasks_width - 13,
                                               command=Task.add_task)
+        self.button_edit_task = tkinter.Button(root, text='Edit task', width=self.listbox_tasks_width - 13,
+                                               command=Task.edit_task)
         self.button_del_task = tkinter.Button(root, text='-Task', width=self.listbox_tasks_width - 13,
                                               command=Task.del_task)
         self.button_save_tasks = tkinter.Button(root, text='Save Tasks', width=self.listbox_tasks_width - 13,
@@ -82,6 +102,7 @@ class Gui:
 
     def button_loader(self):
         self.button_add_task.pack()
+        self.button_edit_task.pack()
         self.button_del_task.pack()
         self.button_save_tasks.pack()
         self.button_load_tasks.pack()
@@ -97,3 +118,5 @@ class Gui:
 Initializer = Gui()
 Initializer.loader()
 root.mainloop()
+
+
